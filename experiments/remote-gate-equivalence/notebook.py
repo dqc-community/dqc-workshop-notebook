f0d2d152a3022c0370343e54a9c0a005a68716f9
Jack Shannon <jack@bosonic.uk>
2026-05-14 10:20:01 +0100

    feat: add explanatory text

--- experiments/remote-gate-equivalence/notebook.py
+++ experiments/remote-gate-equivalence/notebook.py
@@ -24,6 +24,16 @@ def _():
     )
 
 
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    This notebook is a quick exploration/verification of a remote CNOT circuit using our Bell pair remote entanglement primitive and supporting operations.
+
+    First, we define a `remote_circuit` that generates the Bell state $\left\vert \Psi^- \right\rangle = \frac{\left\vert 01 \right\rangle - \left\vert 10 \right\rangle}{\sqrt{2}}$
+    """)
+    return
+
+
 @app.cell
 def _(QuantumCircuit):
     remote_circuit = QuantumCircuit(2, name="remote")
@@ -36,11 +46,29 @@ def _(QuantumCircuit):
     return (remote_circuit,)
 
 
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    Verifying that we generate the correct statevector:
+    """)
+    return
+
+
 @app.cell
 def _(QuantumCircuit, Statevector, remote_circuit):
     psi_minus = QuantumCircuit(2)
     psi_minus.append(remote_circuit.to_gate(), [0, 1])
     Statevector.from_circuit(psi_minus).to_dict()
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    Gate teleportation requires classical communication across modules, which necessitates a measurement on a communication qubit. This creates a branch in the circuit, so we need to build a circuit handling both branches and verify that we get the desired statevector at the end of both circuits.
+
+    First, we build the circuit up to the measurement:
+    """)
     return
 
 
@@ -53,11 +81,27 @@ def _(QuantumCircuit, remote_circuit):
     return (pre,)
 
 
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    Let's inspect the statevector (assuming the circuit is initialized at $\left\vert 0 \right\rangle$):
+    """)
+    return
+
+
 @app.cell
 def _(Statevector, pre):
     psi_pre = Statevector.from_circuit(pre)
     psi_pre.to_dict()
     return (psi_pre,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    We'll be measure `q_1`, the communication qubit in the first module. Given the statevector, we should expect evenly distributed measurement probabilities.
+    """)
+    return
 
 
 @app.cell
@@ -67,25 +111,57 @@ def _(psi_pre):
     return (psi_pre_probs,)
 
 
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    Let's see what happens when we measure the qubit:
+    """)
+    return
+
+
 @app.cell
 def _(psi_pre):
     psi_pre.copy().measure([1])
     return
 
 
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    So we get a tuple `(outcome, statevector)` randomly, depending on the amplitudes. We can use this to construct a dictionary containing the circuit fragments for each measurement outcome:
+    """)
+    return
+
+
 @app.cell
 def _(psi_pre, psi_pre_probs):
     branches = {}
-    while len(branches) < sum([p > 0 for p in psi_pre_probs]):
+    while len(branches) < sum([p > 0 for p in psi_pre_probs]): # ensure we get a measurement for each outcome
         _outcome, _statevector =  psi_pre.copy().measure([1])
         branches[_outcome] = _statevector
     branches
     return (branches,)
 
 
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    Let's inspect the statevector at each branch:
+    """)
+    return
+
+
 @app.cell
 def _(branches):
     {k: v.to_dict() for k, v in branches.items()}
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    Now, we can construct a circuit that gets us to the desired CNOT outcome in both branches:
+    """)
     return
 
 
@@ -113,6 +189,14 @@ def _(branches, post):
     return
 
 
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    Because we don't really care about the state of the communication qubits, we can trace them out of the system. We can then use the resulting density matrix to measure the fidelity relative to the state prepared by a direct CNOT on the data qubits.
+    """)
+    return
+
+
 @app.cell
 def _(DensityMatrix, branches, partial_trace):
     rho = {k: partial_trace(DensityMatrix(v), [1, 3]) for k, v in branches.items()}
@@ -120,8 +204,19 @@ def _(DensityMatrix, branches, partial_trace):
     return
 
 
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## General Testing
+
+    We'll embed everything we did step-by-step into functions so that we have a simulation pipeline that can verify our outcome for an arbitrary initial state of the data qubits.
+    """)
+    return
+
+
 @app.cell
 def _(QuantumCircuit, Statevector):
+    # embed the state of the data qubits into the full system (with communication qubits)
     def embed_state(psi_pre, qubits, n):
         psi_post = Statevector.from_label('0' * n)
         circ = QuantumCircuit(n)
@@ -133,6 +228,7 @@ def _(QuantumCircuit, Statevector):
 
 @app.cell
 def _(DensityMatrix, QuantumCircuit, partial_trace):
+    # perform a direct CX on the data qubits
     def local_cx(psi):
         circ = QuantumCircuit(4)
         circ.cx(0, 2)
@@ -144,6 +240,7 @@ def _(DensityMatrix, QuantumCircuit, partial_trace):
 
 @app.cell
 def _(DensityMatrix, QuantumCircuit, np, partial_trace):
+    # perform a CX on the data qubits using our remote entanglement scheme
     def remote_cx(psi):
         # Define remote circuit
         remote_circuit = QuantumCircuit(2)
@@ -181,6 +278,30 @@ def _(DensityMatrix, QuantumCircuit, np, partial_trace):
     return (remote_cx,)
 
 
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    Qiskit lets you construct statevectors from string labels for convenience. We'll build a list of all combinations of these labels from a few basis states to use in our tests:
+    """)
+    return
+
+
+@app.cell
+def _(itertools):
+    _lablist = '01' # characters to use in our list of state labels
+    labels = [''.join(p) for p in itertools.product(_lablist, _lablist)]
+    labels
+    return (labels,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    We'll first verify that our local CNOT function performs as expected:
+    """)
+    return
+
+
 @app.cell
 def _(Statevector, embed_state, labels, local_cx):
     def _testlab(label):
@@ -189,6 +310,14 @@ def _(Statevector, embed_state, labels, local_cx):
         return local_cx(psi4).to_dict()
 
     {lab: _testlab(lab) for lab in labels}
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    Now we can build a function to compare circuit outcomes for any statevector:
+    """)
     return
 
 
@@ -203,26 +332,35 @@ def _(embed_state, local_cx, remote_cx, state_fidelity):
     return (compare_remote_local,)
 
 
-@app.cell
-def _(Statevector, compare_remote_local):
-    _label = '01'
-    _psi = Statevector.from_label(_label)
-    compare_remote_local(_psi)
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    We feed it our list of state labels to verify the fidelity against the local CNOT:
+    """)
     return
-
-
-@app.cell
-def _(itertools):
-    _lablist = '01'
-    labels = [''.join(p) for p in itertools.product(_lablist, _lablist)]
-    labels
-    return (labels,)
 
 
 @app.cell
 def _(Statevector, compare_remote_local, labels):
     results = {lab: compare_remote_local(Statevector.from_label(lab)) for lab in labels}
     results
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    Success!
+
+    In case you want to experiment further, you can try for any statevector you want:
+    """)
+    return
+
+
+@app.cell
+def _(Statevector, compare_remote_local):
+    _psi = Statevector.from_label('-+')
+    compare_remote_local(_psi)
     return
 
 
