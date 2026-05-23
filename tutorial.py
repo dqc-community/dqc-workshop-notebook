8b2a15a8afe0f00c4a8b9d23caa58ba2f341c8da
Riley O'Shannessy <riley@Rileys-Mac-mini.local>
2026-05-23 20:47:44 +1000

    fix bosonic ghz simulation.
    Note the qiskit design of run_counts has poor design. It has an "automatic" flag for `method` that needs to be manually set. This really should have a None default - maybe worth raising on a PR?

--- tutorial.py
+++ tutorial.py
@@ -631,7 +631,9 @@ def _():
 def verify_ghz_bosonic(n, shots=VERIFY_CFG['SHOTS'], traps=2):
     distributor = bosonic_sdk.BosonicDistributor()
     circuit = compile_bosonic_circuit(ghz_circuit(n), n, traps, distributor)
-    counts, _ = bosonic_sdk.Simulator().run_counts(circuit, ignore_c_remote=True, shots=shots)
+    sim = bosonic_sdk.Simulator()
+    circuit = sim.prepare(circuit)
+    counts, _ = bosonic_sdk.Simulator().run_counts(circuit, ignore_c_remote=True, shots=shots,method="automatic")
     data = {
         'backend': 'Bosonic',
         'n': n,
